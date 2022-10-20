4389b84a1197049667718760d1fa5e6738c55ca6
Ayoub Bouras <aybouras@e1r9p9.1337.ma>
2022-10-20 06:52:38 +0100

    semiFinal..

--- inception/wordpress/tools/launch_wp_script.sh
+++ inception/wordpress/tools/launch_wp_script.sh
@@ -10,17 +10,15 @@ if ! wp core is-installed; then
     # ***************
 
     wp core install --url=$IP_ADDR --title='inception' --admin_user=$WP_ADMIN_USER --admin_email=1337@1337.ma --admin_password=$WP_ADMIN_USER_PASSWORD --path=/var/www/wordpress --allow-root 
-    wp user create $WP_USER inception123@gmail.com --role=subscriber  --user_pass=$WP_USER_PASSWORD  --path=/var/www/wordpress  --allow-root 
+    wp user create $WP_USER inception123@gmail.com --role=subscriber  --user_pass=$WP_USER_PASSWORD  --path=/var/www/wordpress  --allow-root
+    
     # wp theme install twentytwenty --path=/var/www/wordpress  --allow-root  --activate
     wp theme install twentytwentytwo --path=/var/www/wordpress  --allow-root  --activate
-    # wp-content/themes/twentytwentytwo/assets/images/flight-path-on-transparent-d.png
+
     mv /var/www/wordpress/*.png /var/www/wordpress/wp-content/themes/twentytwentytwo/assets/images/
     sed -i "s/'footer-default',/ /" /var/www/wordpress/wp-content/themes/twentytwentytwo/inc/block-patterns.php
 
     wp post create --post_title="I am $IP_ADDR" --post_content="Hi from 1337, the Coding School from Morocco" --post_status=publish --post_author=$WP_USER --path=/var/www/wordpress  --allow-root 
-
-    # SELECT * FROM cloud_db.wp_posts WHERE wp_posts.post_name = 'hello-world'; 
-    # DELETE FROM cloud_db.wp_posts WHERE wp_posts.post_name = 'hello-world'
 
      echo "DELETE FROM cloud_db.wp_posts WHERE wp_posts.post_name = 'hello-world';" | mariadb -h mariadb -P$DB_PORT -u$DB_USER -p$DB_PASSWORD
 fi
